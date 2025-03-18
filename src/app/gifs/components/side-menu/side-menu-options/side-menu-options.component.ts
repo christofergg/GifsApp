bfdc33565ec47a0746ca87f68edd49dc61d8b71e
Christofer Gomez <christofergg@gmail.com>
2025-03-18 21:58:39 +0100

    Side-menu, side-menu-header and side-menu-options

---
+++ src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gifs-side-menu-options',
+  imports: [],
+  templateUrl: './side-menu-options.component.html',
+  styleUrl: './side-menu-options.component.css'
+})
+export class SideMenuOptionsComponent {
+
+}
