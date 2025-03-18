4919baa9e6f284fc04904054bb9f24a837b58145
Christofer Gomez <christofergg@gmail.com>
2025-03-18 22:24:56 +0100

    fontawsome and customize side-menu-options, RouterLink and RouterLinkActive

--- src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
+++ src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuOption {
+  label: string,
+  subLabel: string,
+  icon: string,
+  route: string
+}
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu-options.component.html',
   styleUrl: './side-menu-options.component.css'
 })
 export class SideMenuOptionsComponent {
-
+   menuOptions: MenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Popular Gifs',
+      route: '/dashboard/trending'
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Search',
+      subLabel: 'Search gifs',
+      route: '/dashboard/search'
+    }
+  ]
 }
