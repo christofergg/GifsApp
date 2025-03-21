981ab4daec39093e9e3064dfe1a14fb4a722229f
Christofer Gomez <christofergg@gmail.com>
2025-03-21 17:10:04 +0100

    HttpClient and providerHttpClient WithFetch

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,32 @@
+import { GiphyDTOMapper } from './../mapper/giphyDTO.mapper';
+import { GiphyDTO } from './../interfaces/giphyDTO.interface';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { environment } from '@environments/environment';
+import type { GiphyResponse } from '../interfaces/giphy.interfaces';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+
+  private http = inject(HttpClient);
+
+  trendingGifs = signal<GiphyDTO[]>([])
+
+  constructor() {
+    this.loadTrendingGifs();
+   }
+  loadTrendingGifs() {
+    this.http.get<GiphyResponse>(`${ environment.giphyServiceUrl }/trending`, {
+      params: {
+        api_key: environment.giphyApiToken,
+        limit: 25
+      }
+    }).subscribe((response) => {
+      const gifs = GiphyDTOMapper.mapGiphyItemToGiphyDTOArray(response.data);
+      this.trendingGifs.set(gifs);
+      console.log({gifs});
+    });
+  }
+}
