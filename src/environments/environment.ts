35dc4da3dd8f0d6c81f16412d67bb0967e4527bb
Christofer Gomez <christofergg@gmail.com>
2025-03-21 17:10:26 +0100

    Add token and urlservice for giphy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,7 @@ export const environment = {
   productions : true,
   companyName: 'Gifs',
   companyNameRemark: 'App',
-  companySlogan: 'beautiful App'
+  companySlogan: 'beautiful App',
+  giphyApiToken: 'sLfuwzOjG3QvqoJrv2jJamEnRRTWwABQ',
+  giphyServiceUrl: 'https://api.giphy.com/v1/gifs'
 };
